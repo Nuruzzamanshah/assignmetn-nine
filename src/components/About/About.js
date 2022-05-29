ee11a32b11ba666f85d73994a3928a125b7663c1
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-29 21:01:31 +0600

    about page added and style add

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  './About.css'
+
+const About = () => {
+    return (
+        
+        <div className='about'>
+            <div>
+                <h1 className='text'>the book all about</h1>
+            </div>
+            <h3>The Old Man and the Sea is a classic novel written in 1951 by Ernest Hemingway. It is considered to be the last major work by the eminent author to be published while he was alive.
+            The book is a short read (under 100 pages) which is set in Havana, Cuba. The story tells us the tale of an old fisherman, a young boy and a beautiful and brave fish.</h3>
+        </div>
+    );
+};
+
+export default About;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
       <div>
-        <nav className='mt-5'>
+        <nav className='mt-5 nav'>
           <Link className="mr-5 text-bold" to="/home">
             HOME
           </Link>
